b48e92cf0d428c6f12d2f6ef7a772e85788c6af6
jackscott1 <jack.scott@monash.edu>
2021-09-30 23:52:27 +1000

    added reversal

--- db_insert_dev.sql
+++ db_insert_dev.sql
@@ -1,9 +1,9 @@
 INSERT INTO animal(animal_code) VALUES('MSIMD-2123');
-
+INSERT INTO protocol(protocol_name) VALUES('reversal');
 INSERT INTO protocol(protocol_name) VALUES('dmts');
 INSERT INTO protocol(protocol_name) VALUES('supertask');
 INSERT INTO protocol(protocol_name) VALUES('arrow_test');
 INSERT INTO protocol(protocol_name) VALUES('levelone');
 INSERT INTO protocol(protocol_name) VALUES('leveltwo');
 INSERT INTO protocol(protocol_name) VALUES('levelthree');
-INSERT INTO protocol(protocol_name) VALUES ('arrow_search')
+INSERT INTO protocol(protocol_name) VALUES('arrow_search');
