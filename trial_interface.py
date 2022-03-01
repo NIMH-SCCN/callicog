ff3761af005a43fc4d1cda6aeeed6dc1ac8cef6d
jackscott1 <jack.scott@monash.edu>
2022-03-01 12:00:06 +1100

    conversion to rgb255 for more control

--- trial_interface.py
+++ trial_interface.py
@@ -8,9 +8,9 @@ from serial import SerialException
 class WindowRuntime:
 	def __get_ppy_stim_from_shape(self, shape, ppy_window):
 		if shape == StimulusShape.SQUARE:
-			return visual.Rect(win=ppy_window)
+			return visual.Rect(win=ppy_window, colorSpace='rgb255')
 		elif shape == StimulusShape.CIRCLE:
-			return visual.Circle(win=ppy_window, colorSpace='rgb')
+			return visual.Circle(win=ppy_window, colorSpace='rgb255')
 		elif shape == StimulusShape.STAR:
 			star_vertices = []
 			outer_radius = 131
@@ -21,38 +21,38 @@ class WindowRuntime:
 				star_vertices.append([x,y]); x = inner_radius*math.cos(math.radians(126+vertex*72))
 				y = inner_radius*math.sin(math.radians(126+vertex*72))
 				star_vertices.append([x,y])
-			return visual.ShapeStim(win=ppy_window, vertices=star_vertices, units = 'pix', colorSpace='rgb')
+			return visual.ShapeStim(win=ppy_window, vertices=star_vertices, units = 'pix', colorSpace='rgb255')
 		elif shape == StimulusShape.DIAMOND:
-			return visual.Rect(win=ppy_window, ori=45, colorSpace='rgb')
+			return visual.Rect(win=ppy_window, ori=45, colorSpace='rgb255')
 		elif shape == StimulusShape.ARROW_N:
 			arrow_vertices = [(0,4), (-3,0), (-1,0), (-1,-3), (1,-3), (1,0), (3,0)]
-			return visual.ShapeStim(win=ppy_window, vertices=arrow_vertices, colorSpace='rgb')
+			return visual.ShapeStim(win=ppy_window, vertices=arrow_vertices, colorSpace='rgb255')
 		elif shape == StimulusShape.IMAGE:
-			return visual.ImageStim(win=ppy_window, colorSpace='rgb')
+			return visual.ImageStim(win=ppy_window, colorSpace='rgb255')
 		elif shape == StimulusShape.ARROW_E:
 			arrow_vertices = [(4, 0), (0,-3), (0,-1), (-3,-1), (-3, 1), (0, 1), (0, 3)]
-			return visual.ShapeStim(win=ppy_window, vertices=arrow_vertices, colorSpace='rgb')
+			return visual.ShapeStim(win=ppy_window, vertices=arrow_vertices, colorSpace='rgb255')
 		elif shape == StimulusShape.ARROW_W:
 			arrow_vertices = [(-4, 0), (0,-3), (0,-1), (3,-1), (3, 1), (0, 1), (0, 3)]
-			return visual.ShapeStim(win=ppy_window, vertices=arrow_vertices, colorSpace='rgb')
+			return visual.ShapeStim(win=ppy_window, vertices=arrow_vertices, colorSpace='rgb255')
 		elif shape == StimulusShape.ARROW_S:
 			arrow_vertices = [(0,-4), (-3,0), (-1,0), (-1,3), (1,3), (1,0), (3,0)]
-			return visual.ShapeStim(win=ppy_window, vertices=arrow_vertices, colorSpace='rgb')
+			return visual.ShapeStim(win=ppy_window, vertices=arrow_vertices, colorSpace='rgb255')
 		elif shape == StimulusShape.TRIANGLE:
 			triangle_vertices = [(-5,4), (5,4), (0, -4)]
-			return visual.ShapeStim(win=ppy_window, vertices=triangle_vertices, colorSpace='rgb')
+			return visual.ShapeStim(win=ppy_window, vertices=triangle_vertices, colorSpace='rgb255')
 		elif shape == StimulusShape.ARROW_NE:
 			arrow_vertices = [(2.83,2.83), (-2.12,2.12), (-0.71,0.71), (-2.83,-1.41), (-1.41,-2.83), (0.71,-0.71), (2.12,-2.12)]
-			return visual.ShapeStim(win=ppy_window, vertices=arrow_vertices, colorSpace='rgb')
+			return visual.ShapeStim(win=ppy_window, vertices=arrow_vertices, colorSpace='rgb255')
 		elif shape == StimulusShape.ARROW_NW:
 			arrow_vertices = [(-2.83,2.83), (2.12,2.12), (0.71,0.71), (2.83,-1.41), (1.41,-2.83), (-0.71,-0.71), (-2.12,-2.12)]
-			return visual.ShapeStim(win=ppy_window, vertices=arrow_vertices, colorSpace='rgb')
+			return visual.ShapeStim(win=ppy_window, vertices=arrow_vertices, colorSpace='rgb255')
 		elif shape == StimulusShape.ARROW_SE:
 			arrow_vertices = [(2.83,-2.83), (-2.12,-2.12), (-0.71,-0.71), (-2.83,1.41), (-1.41,2.83), (0.71,0.71), (2.12,2.12)]
-			return visual.ShapeStim(win=ppy_window, vertices=arrow_vertices, colorSpace='rgb')
+			return visual.ShapeStim(win=ppy_window, vertices=arrow_vertices, colorSpace='rgb255')
 		elif shape == StimulusShape.ARROW_SW:
 			arrow_vertices = [(-2.83,-2.83), (2.12,-2.12), (0.71,-0.71), (2.83,1.41), (1.41,2.83), (-0.71,0.71), (-2.12,2.12)]
-			return visual.ShapeStim(win=ppy_window, vertices=arrow_vertices, colorSpace='rgb')
+			return visual.ShapeStim(win=ppy_window, vertices=arrow_vertices, colorSpace='rgb255')
 
 	def __load_stimulus(self, stimulus, ppy_window):
 		stimulus.ppy_touch_stim = visual.Rect(win=ppy_window, opacity=0)
