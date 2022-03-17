835d20e1c4eef833b3363ec77ba77defee6a4125
jackscott1 <jack.scott@monash.edu>
2022-03-17 17:20:52 +1100

    revert back to rgb colorspace

--- tasks/dmts_train3.py
+++ tasks/dmts_train3.py
@@ -9,8 +9,8 @@ class TaskInterface(TaskStructure):
 		self.init_parameters()
 
 	def init_parameters(self):
-		red_diamond = Stimulus(shape=StimulusShape.DIAMOND, size=(176.78,176.78), color=(255,0,0), size_touch=(250,250))
-		yellow_circle = Stimulus(shape=StimulusShape.CIRCLE, size = (250,250), color=(255,255,0), size_touch=(250,250)) #check size
+		red_diamond = Stimulus(shape=StimulusShape.DIAMOND, size=(176.78,176.78), color=(1,-1,-1), size_touch=(250,250))
+		yellow_circle = Stimulus(shape=StimulusShape.CIRCLE, size = (250,250), color=(1,1,-1), size_touch=(250,250)) #check size
 		stimulus_list = [red_diamond, yellow_circle]
 
 		# Positions are listed as [sample_pos,distractor_pos]
