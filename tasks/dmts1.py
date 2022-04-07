ce48ece0085acb2915723ebda66a8b81e42b896e
jackscott1 <jack.scott@monash.edu>
2022-04-07 16:13:12 +1000

    2 s timeout added to 2nd stimulus window

--- tasks/dmts1.py
+++ tasks/dmts1.py
@@ -39,7 +39,7 @@ class TaskInterface(TaskStructure):
 		w2 = Window(blank=0.5)
 
 		# Window 3
-		w3 = Window(transition=WindowTransition.RELEASE)
+		w3 = Window(transition=WindowTransition.RELEASE, timeout=2)
 		w3_sample = copy.copy(trial_parameters[Parameter.TARGET])
 		w3_sample.position = (0,0)
 		w3.add_stimulus(w3_sample)
