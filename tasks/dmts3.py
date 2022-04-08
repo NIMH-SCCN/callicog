6e1bb8d28113554864892be07d54515c99e555bd
jackscott1 <jack.scott@monash.edu>
2022-04-08 12:33:30 +1000

    test

--- tasks/dmts3.py
+++ tasks/dmts3.py
@@ -39,7 +39,7 @@ class TaskInterface(TaskStructure):
 		w2 = Window(blank=0.5) 
 
 		# Window 3
-		w3 = Window(transition=WindowTransition.RELEASE, label = 'encoding')
+		w3 = Window(transition=WindowTransition.RELEASE, label = 'encoding', timeout=2, is_outcome=True)
 		w3_sample = copy.copy(trial_parameters[Parameter.TARGET])
 		w3_sample.position = (0,0)
 		w3.add_stimulus(w3_sample)
