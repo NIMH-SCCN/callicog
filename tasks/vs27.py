fdf05127486c41e97472b88356a0bf8060c12c9e
angelafan1 <yuxin.fan@monash.edu>
2022-05-30 16:04:22 +1000

    testing vs17

--- tasks/vs27.py
+++ tasks/vs27.py
@@ -11,7 +11,7 @@ class TaskInterface(TaskStructure):
 		self.init_parameters()
 
 	def init_parameters(self):
-		ndistractors_list = [13]
+		ndistractors_list = [13, 10, 5]
 		positions_list = [(-466, 233), (-233, 233), (0, 233), (233, 233), (466, 233), (-466, 0), (-233, 0), (0, 0), (233, 0), (466, 0),(-466, -233), (-233, -233), (0, -233), (233, -233), (466, -233)]
 		blue_triangle = Stimulus(shape=StimulusShape.TRIANGLE,
 					size=(15,15),
@@ -100,7 +100,7 @@ class TaskInterface(TaskStructure):
 		
 		blueshapes = [blue_triangle, blue_star, blue_circle, blue_diamond, blue_triangle, blue_star, blue_circle, blue_diamond]
 		yellowshapes = [yellow_triangle, yellow_star, yellow_diamond, yellow_circle,yellow_triangle, yellow_star, yellow_diamond, yellow_circle,]
-		bluearrows = [blue_arrow_s, blue_arrow_n, blue_arrow_n, blue_arrow_s, blue_arrow_ne, blue_arrow_nw, blue_arrow_ne, blue_arrow_nw, blue_arrow_se, blue_arrow_sw, blue_arrow_se, blue_arrow_sw, blue_arrow_e, blue_arrow_s, blue_arrow_e, blue_arrow_s ]
+		bluearrows = [blue_arrow_s, blue_arrow_n, blue_arrow_e, blue_arrow_n, blue_arrow_ne, blue_arrow_nw, blue_arrow_se, blue_arrow_sw,]
 		yellowarrows = [yellow_arrow_s, yellow_arrow_s, yellow_arrow_ne,yellow_arrow_ne,yellow_arrow_nw, yellow_arrow_nw, yellow_arrow_se, yellow_arrow_se, yellow_arrow_sw, yellow_arrow_sw]
 
 		self.add_parameter(Parameter.BLUESHAPES, blueshapes, pseudorandom=False)
@@ -139,12 +139,30 @@ class TaskInterface(TaskStructure):
 		# set distractors
 		distractor_positions = self.randomize_from(self.pseudorandom_parameters[Parameter.POSITION]['values'], exclude=[trial_parameters[Parameter.POSITION]], size=trial_parameters[Parameter.DISTRACTOR_NUMBER])
 		if len(distractor_positions) == 13:
-				for position in distractor_positions:
+			for position in distractor_positions:
+				blue_dis = self.randomize_from(self.pseudorandom_parameters[Parameter.YELLOWARROWS]['values'], size=7)
+				yellow_dis = self.randomize_from(self.pseudorandom_parameters[Parameter.BLUEARROWS]['values'], size=6)
+				distractor = blue_dis+yellow_dis
+				print(distractor)
+				distractor_stim = copy.copy(distractor[0])
+				distractor_stim.position = position
+				distractor_stim.outcome = Outcome.FAIL
+				w3.add_stimulus(distractor_stim)
+		elif len(distractor_positions) == 10: 
+			for position in distractor_positions:
 					blue_dis = self.randomize_from(self.pseudorandom_parameters[Parameter.YELLOWARROWS]['values'], size=7)
 					yellow_dis = self.randomize_from(self.pseudorandom_parameters[Parameter.BLUEARROWS]['values'], size=6)
 					distractor = blue_dis+yellow_dis
 					print(distractor)
 					distractor_stim = copy.copy(distractor[0])
+					distractor_stim.position = position
+					distractor_stim.outcome = Outcome.FAIL
+					w3.add_stimulus(distractor_stim)
+		else: 
+			for position in distractor_positions:
+					yellow_dis = self.randomize_from(self.pseudorandom_parameters[Parameter.YELLOWARROWS]['values'], size=len(distractor_positions))
+					print(distractor)
+					distractor_stim = copy.copy(yellow_dis[0])
 					distractor_stim.position = position
 					distractor_stim.outcome = Outcome.FAIL
 					w3.add_stimulus(distractor_stim)
