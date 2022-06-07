c35b86788b4d8675848f830906ea04c3017d8815
angelafan1 <yuxin.fan@monash.edu>
2022-06-07 11:51:15 +1000

    yellow arrow

--- tasks/touchtraining8.py
+++ tasks/touchtraining8.py
@@ -26,7 +26,7 @@ class TaskInterface(TaskStructure):
 		# Window 3
 		# set targets
 		w3 = Window(transition=WindowTransition.TOUCH, is_outcome=True, timeout=5, is_outside_fail=True)
-		target_stim = Stimulus(shape=StimulusShape.ARROW_N, size=(25,25), color = (1, 1, 0), size_touch=(160,160))
+		target_stim = Stimulus(shape=StimulusShape.ARROW_N, size=(25,25), color = (255, 255, 0), size_touch=(160,160))
 		target_stim.position = (random.randint(-520, 520), random.randint(-220, 220))
 		target_stim.outcome = Outcome.SUCCESS
 		w3.add_stimulus(target_stim)
