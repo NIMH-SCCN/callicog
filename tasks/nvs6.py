16f24b91f42e96c01db06c39aede52468bb7a69f
angelafan1 <yuxin.fan@monash.edu>
2022-06-13 15:27:36 +1000

    ew tasks

--- tasks/nvs6.py
+++ tasks/nvs6.py
@@ -10,126 +10,123 @@ class TaskInterface(TaskStructure):
 		self.init_parameters()
 
 	def init_parameters(self):
-		ndistractors_list = [6, 7, 8]
+		ndistractors_list = [1,2,3,4]
 		positions_list = [(-466, 233), (-233, 233), (0, 233), (233, 233), (466, 233), (-466, 0), (-233, 0), (0, 0), (233, 0), (466, 0),(-466, -233), (-233, -233), (0, -233), (233, -233), (466, -233)]
 		
 		blue_triangle = Stimulus(shape=StimulusShape.TRIANGLE,
 					size=(15,15),
-					color=(0, 255, 255),
+					color=(120, 216, 255),
 					size_touch= (160,160))
 		blue_star = Stimulus(shape=StimulusShape.STAR,
 					size=(0.7,0.7),
-					color=(0, 255, 255),
+					color=(120, 216, 255),
 					size_touch= (160,160))
 		blue_circle = Stimulus(shape=StimulusShape.CIRCLE,
 					size=(140,140),
-					color=(0, 255, 255),
+					color=(120, 216, 255),
 					size_touch= (160,160))
 		yellow_circle = Stimulus(shape=StimulusShape.CIRCLE,
 					size=(140,140),
-					color=(220,220,0),
+					color=(255, 255, 100),
 					size_touch= (160,160))
 		blue_diamond = Stimulus(shape=StimulusShape.DIAMOND,
 					size=(120,120),
-					color=(0, 255, 255),
+					color=(120, 216, 255),
 					size_touch= (160,160))
 		yellow_triangle = Stimulus(shape=StimulusShape.TRIANGLE,
 					size=(15,15),
-					color=(220,220,0),
+					color=(255, 255, 100),
 					size_touch= (160,160))
 		blue_arrow_e = Stimulus(shape=StimulusShape.ARROW_E,
 					size=(25,25), 
-					color=(0, 255, 255),
+					color = (120, 216, 255),
 					size_touch=(160,160))
 		blue_arrow_w = Stimulus(shape=StimulusShape.ARROW_W,
 					size=(25,25), 
-					color=(0, 255, 255),
+					color = (120, 216, 255),
 					size_touch=(160,160))
 		yellow_star = Stimulus(shape=StimulusShape.STAR,
 					size=(0.7,0.7),
-					color=(220,220,0),
+					color=(255, 255, 100),
 					size_touch= (160,160))
 		yellow_diamond = Stimulus(shape=StimulusShape.DIAMOND,
 					size=(120,120),
-					color=(220,220,0),
+					color=(255, 255, 100),
 					size_touch= (160,160))
 		yellow_arrow_ne = Stimulus(shape=StimulusShape.ARROW_NE,
 					size=(25,25), 
-					color=(220,220,0),
+					color = (255, 255, 100),
 					size_touch=(160,160))
 		blue_arrow_ne = Stimulus(shape=StimulusShape.ARROW_NE,
 					size=(25,25), 
-					color=(0, 255, 255),
+					color = (120, 216, 255),
 					size_touch=(160,160))
 		yellow_arrow_nw = Stimulus(shape=StimulusShape.ARROW_NW,
 					size=(25,25), 
-					color=(220,220,0),
+					color = (255, 255, 100),
 					size_touch=(160,160))
 		blue_arrow_nw = Stimulus(shape=StimulusShape.ARROW_NW,
 					size=(25,25), 
-					color=(0, 255, 255),
+					color = (120, 216, 255),
 					size_touch=(160,160))
 		yellow_arrow_se = Stimulus(shape=StimulusShape.ARROW_SE,
 					size=(25,25), 
-					color=(220,220,0),
+					color = (255, 255, 100),
 					size_touch=(160,160))
 		yellow_arrow_w = Stimulus(shape=StimulusShape.ARROW_W,
 					size=(25,25), 
-					color=(220,220,0),
+					color = (255, 255, 100),
 					size_touch=(160,160))
 		yellow_arrow_e = Stimulus(shape=StimulusShape.ARROW_E,
 					size=(25,25), 
-					color=(220,220,0),
+					color = (255, 255, 100),
 					size_touch=(160,160))
 		yellow_arrow_sw = Stimulus(shape=StimulusShape.ARROW_SW,
 					size=(25,25), 
-					color=(220,220,0),
+					color = (255, 255, 100),
 					size_touch=(160,160))
 		blue_arrow_sw = Stimulus(shape=StimulusShape.ARROW_SW,
 					size=(25,25), 
-					color=(0, 255, 255),
+					color = (120, 216, 255),
 					size_touch=(160,160))
 		blue_arrow_s = Stimulus(shape=StimulusShape.ARROW_S,
 					size=(25,25), 
-					color=(0, 255, 255),
+					color = (120, 216, 255),
 					size_touch=(160,160))
 		yellow_arrow_s = Stimulus(shape=StimulusShape.ARROW_S,
 					size=(25,25), 
-					color=(220,220,0),
+					color = (255, 255, 100),
 					size_touch=(160,160))
 		blue_arrow_n = Stimulus(shape=StimulusShape.ARROW_N,
 					size=(25,25), 
-					color=(0, 255, 255),
+					color = (120, 216, 255),
 					size_touch=(160,160))
 		blue_arrow_se = Stimulus(shape=StimulusShape.ARROW_SE,
 					size=(25,25), 
-					color=(0, 255, 255),
+					color = (120, 216, 255),
 					size_touch=(160,160))
 		blue_triangle = Stimulus(shape=StimulusShape.TRIANGLE,
 					size=(15,15),
-					color=(0, 255, 255),
+					color=(0, 0.7, 1),
 					size_touch= (160,160))
 		blue_star = Stimulus(shape=StimulusShape.STAR,
 					size=(0.7,0.7),
-					color=(0, 255, 255),
+					color=(0, 0.7, 1),
 					size_touch= (160,160))
 		blue_circle = Stimulus(shape=StimulusShape.CIRCLE,
 					size=(140,140),
-					color=(0, 255, 255),
+					color=(0, 0.7, 1),
 					size_touch= (160,160))
 		blue_diamond = Stimulus(shape=StimulusShape.DIAMOND,
 					size=(120,120),
-					color=(0, 255, 255),
+					color=(0, 0.7, 1),
 					size_touch= (160,160))
-
 		blueshapes = [blue_triangle, blue_star, blue_circle, blue_diamond, blue_triangle, blue_star, blue_circle, blue_diamond, blue_triangle, blue_star, blue_circle, blue_diamond]
-		bluearrowsyellowshapes = [blue_arrow_e, blue_arrow_n, blue_arrow_nw, blue_arrow_se, blue_arrow_sw, blue_arrow_s, blue_arrow_sw, blue_arrow_ne, blue_arrow_w, yellow_circle, yellow_star, yellow_triangle, yellow_star, yellow_circle, yellow_star, yellow_triangle, yellow_star]
-		bluearrowsyellowarrows = [blue_arrow_e, blue_arrow_n, blue_arrow_nw, blue_arrow_se, blue_arrow_sw, blue_arrow_sw, blue_arrow_ne, blue_arrow_w, yellow_arrow_ne, yellow_arrow_nw, yellow_arrow_s, yellow_arrow_se, yellow_arrow_sw, yellow_arrow_e, yellow_arrow_w]
-		yellowarrows=[yellow_arrow_ne, yellow_arrow_nw, yellow_arrow_s, yellow_arrow_se, yellow_arrow_sw, yellow_arrow_e, yellow_arrow_w, yellow_arrow_ne, yellow_arrow_nw, yellow_arrow_s, yellow_arrow_se, yellow_arrow_sw, yellow_arrow_e, yellow_arrow_w, yellow_arrow_s]
+		bluearrowsyellowshapes = [blue_arrow_e, blue_arrow_n, blue_arrow_nw, blue_arrow_se, blue_arrow_sw, blue_arrow_s, blue_arrow_sw, blue_arrow_w, yellow_circle, yellow_star, yellow_triangle, yellow_star, yellow_circle, yellow_star, yellow_triangle, yellow_star]
+		bluearrowsyellowarrows = [blue_arrow_e, blue_arrow_n, blue_arrow_nw, blue_arrow_se, blue_arrow_sw, blue_arrow_sw, blue_arrow_w, yellow_arrow_ne, yellow_arrow_nw, yellow_arrow_s, yellow_arrow_se, yellow_arrow_sw, yellow_arrow_e, yellow_arrow_w]
 		self.add_parameter(Parameter.BLUESHAPES, blueshapes, pseudorandom= True)
 		self.add_parameter(Parameter.BLUEARROWSYELLOWSHAPES, bluearrowsyellowshapes, pseudorandom= True)
 		self.add_parameter(Parameter.BLUEARROWSYELLOWARROWS, bluearrowsyellowarrows, pseudorandom= True)
-		self.add_parameter(Parameter.YELLOWARROWS, yellowarrows, pseudorandom= True)
 		self.add_parameter(Parameter.DISTRACTOR_NUMBER, ndistractors_list)
 		self.add_parameter(Parameter.POSITION, positions_list)
 
@@ -150,10 +147,10 @@ class TaskInterface(TaskStructure):
 
 		# Window 3
 		# set targets
-		w3 = Window(transition=WindowTransition.TOUCH, is_outcome=True, timeout=2)
+		w3 = Window(transition=WindowTransition.TOUCH, is_outcome=True, timeout=3)
 		target_stim = Stimulus(shape=StimulusShape.ARROW_N,
 				size=(25,25), 
-				color = (220, 220, 0),
+				color = (255, 255, 100),
 				size_touch=(160,160))
 		target_stim.position = trial_parameters[Parameter.POSITION]
 		target_stim.outcome = Outcome.SUCCESS
@@ -161,30 +158,28 @@ class TaskInterface(TaskStructure):
 		
 		# set distractors
 		distractor_positions = self.randomize_from(self.pseudorandom_parameters[Parameter.POSITION]['values'], exclude=[trial_parameters[Parameter.POSITION]], size=trial_parameters[Parameter.DISTRACTOR_NUMBER])
-		if len(distractor_positions) == 6:
+		if len(distractor_positions) == 3:
 				for position in distractor_positions:
-					distractor = self.randomize_from(self.pseudorandom_parameters[Parameter.BLUEARROWSYELLOWARROWS]['values'], size=6)
+					distractor = self.randomize_from(self.pseudorandom_parameters[Parameter.BLUEARROWSYELLOWSHAPES]['values'], size=3)
 					distractor_stim = copy.copy(distractor[0])
 					distractor_stim.position = position
 					distractor_stim.outcome = Outcome.FAIL
 					w3.add_stimulus(distractor_stim)
 				
-		elif len(distractor_positions) == 7:
+		if len(distractor_positions) == 4:
 				for position in distractor_positions:
-					distractor = self.randomize_from(self.pseudorandom_parameters[Parameter.BLUEARROWSYELLOWSHAPES]['values'], size=7)
+					distractor = self.randomize_from(self.pseudorandom_parameters[Parameter.BLUESHAPES]['values'], size=2)
 					distractor_stim = copy.copy(distractor[0])
 					distractor_stim.position = position
 					distractor_stim.outcome = Outcome.FAIL
 					w3.add_stimulus(distractor_stim)
-
 		else:
 				for position in distractor_positions:
-					distractor = self.randomize_from(self.pseudorandom_parameters[Parameter.BLUESHAPES]['values'], size=8)
-					distractor_stim = copy.copy(distractor[0])
+					distractor2 = self.randomize_from(self.pseudorandom_parameters[Parameter.BLUEARROWSYELLOWARROWS]['values'], size=len(distractor_positions))
+					distractor_stim = copy.copy(distractor2[0])
 					distractor_stim.position = position
 					distractor_stim.outcome = Outcome.FAIL
-					w3.add_stimulus(distractor_stim)
-
+					w3.add_stimulus(distractor_stim) 
 
 		# Window 8
 		w4 = Window(blank=2)
