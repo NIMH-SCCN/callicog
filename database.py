e0e04699d160343bd2635678578727a05c556ba2
Brian Stewart <brian.stewart@nih.gov>
2023-08-22 15:39:27 -0400

    Enlarge connection pool size
    This is a temporary fix to resolve webapp crashes. Probably the
    connections are not being recycled into the pool correctly. Hopefully
    this will patch sufficiently to reduce webapp crashes for now.

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import (
 )
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('postgresql:///marmodb', echo=False)
+engine = create_engine('postgresql:///marmodb', echo=False, pool_size=20)
 DatabaseSession = sessionmaker()
 DatabaseSession.configure(bind=engine)
 Base = declarative_base()
