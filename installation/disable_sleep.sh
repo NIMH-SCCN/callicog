53113396540bd7fb919730981ea7f1ed99885b3a
Brian Stewart <brian.stewart@nih.gov>
2023-09-01 14:20:53 -0400

    Only attempt to disable sleep if display attached

--- installation/disable_sleep.sh
+++ installation/disable_sleep.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-# Disable screen saver and DPMS
-xset s off
-xset -dpms
+# Check if DISPLAY is set (X server is running)
+if [ -n "$DISPLAY" ]; then
+    # Disable screen saver
+    xset s off
+
+    # Disable DPMS
+    xset -dpms
+fi
 
 # NOTE: if display is still sleeping/suspending, try running this command:
-# 
+#
 #   sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
 #
 # This command will "mask" or disable these settings persistently until they
