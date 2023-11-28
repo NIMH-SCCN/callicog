1a28f1ef226f7a7be88e0920e27eb9c0728d82d3
Brian Stewart <bstewart@usnews.com>
2023-11-28 10:46:01 -0500

    Tweak backup script

--- scripts/backup_db.sh
+++ scripts/backup_db.sh
@@ -1,9 +1,21 @@
 #!/bin/zsh
 
+# Check if an argument is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <output_directory>"
+    exit 1
+fi
+
 # Define environment variables for username, host, and database
 PG_USER="sccn"
 PG_HOST="localhost"
 PG_DB="marmodb"
+
+# Get the current date in YYYY_MM_DD format
+CURRENT_DATE=$(date +"%Y_%m_%d")
+
+# Define the output directory with the current date
+OUTPUT_DIR="$(pwd)/$1/$PG_DB/$CURRENT_DATE"
 
 # Define the SQL command to select tables
 SQL_COMMAND=$(cat <<-SQL
@@ -14,12 +26,6 @@ SQL_COMMAND=$(cat <<-SQL
 SQL
 )
 
-# Get the current date in YYYY_MM_DD format
-CURRENT_DATE=$(date +"%Y_%m_%d")
-
-# Define the output directory with the current date
-OUTPUT_DIR="Users/sccn/db_backup/marmodb/$CURRENT_DATE"
-
 # Check if the output directory already exists
 if [ -d "$OUTPUT_DIR" ]; then
     echo "Error: The output directory '$OUTPUT_DIR' already exists. Aborting."
@@ -29,11 +35,14 @@ fi
 # Create the output directory
 mkdir -p "$OUTPUT_DIR"
 
+echo "Backing up '$PG_DB' to $OUTPUT_DIR"
+
 # Iterate through tables
 for table in $(psql -U "$PG_USER" -h "$PG_HOST" -d "$PG_DB" -t -c "$SQL_COMMAND");
 do
     # Dump the table to a SQL file in the specified output directory
     pg_dump -t "$table" -U "$PG_USER" -h "$PG_HOST" "$PG_DB" > "$OUTPUT_DIR/$table.sql"
+    #echo "Backing up $table..."
 done;
 
 # Dump schema only
