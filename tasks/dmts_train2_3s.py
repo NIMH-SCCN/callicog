675446c2164f7922f9851f4bddff23eb770e39f5
Brian Stewart <bstewart@usnews.com>
2024-01-10 12:05:13 -0500

    new tasks

---
+++ tasks/dmts_train2_3s.py
@@ -0,0 +1,77 @@
+from task_builder import Window, Stimulus, WindowTransition, StimulusShape, Outcome, Parameter
+from task_structure import TaskStructure
+import random
+import copy
+
+class TaskInterface(TaskStructure):
+	def __init__(self):
+		super().__init__()
+		self.init_parameters()
+
+	def init_parameters(self):
+		red_diamond = Stimulus(shape=StimulusShape.DIAMOND, size=(176.78,176.78), color=(1,-1,-1), size_touch=(250,250))
+		yellow_circle = Stimulus(shape=StimulusShape.CIRCLE, size = (250,250), color=(1,1,-1), size_touch=(250,250)) #check size
+		stimulus_list = [red_diamond, yellow_circle]
+		position_list = [(-382.5, 0), (382.5, 0)]
+		
+		self.add_parameter(Parameter.POSITION, position_list)
+		self.add_parameter(Parameter.TARGET, stimulus_list)		
+
+	def generate_trials(self):
+		self.trials = self.pseudorandomize_parameters()
+
+	def build_trial(self, trial_parameters={}):
+		# Window 1
+		w1 = Window(transition=WindowTransition.TOUCH)
+		w1_sample = copy.copy(trial_parameters[Parameter.TARGET])
+		w1_sample.position = (0,0)
+		w1.add_stimulus(w1_sample)
+
+		# Window 2
+		w2 = Window(blank=0.5)
+
+		# Window 3
+		w3 = Window(transition=WindowTransition.RELEASE)
+		w3_sample = copy.copy(trial_parameters[Parameter.TARGET])
+		w3_sample.position = (0,0)
+		w3.add_stimulus(w3_sample)
+
+		# Window 4
+		w4 = Window(blank=0.5)
+  
+		# Window 5
+		w5 = Window(transition=WindowTransition.RELEASE)
+		w5_sample = copy.copy(trial_parameters[Parameter.TARGET])
+		w5_sample.position = (0,0)
+		w5.add_stimulus(w5_sample)
+
+		# Window 6
+		w6 = Window(blank=0.5)
+
+		# Window 7
+		w7 = Window(transition=WindowTransition.TOUCH, is_outcome=True, timeout=3)
+		w7.post_touch_delay = 1
+		w7_sample = copy.copy(trial_parameters[Parameter.TARGET])
+		w7_sample.after_touch = [{'name': 'hide_other'}]
+		w7_sample.position = trial_parameters[Parameter.POSITION]
+		w7_sample.outcome = Outcome.SUCCESS
+		
+		w7_distractor = self.randomize_from(self.pseudorandom_parameters[Parameter.TARGET]['values'], exclude=[trial_parameters[Parameter.TARGET]])[0]
+		w7_distractor.position = self.randomize_from(self.pseudorandom_parameters[Parameter.POSITION]['values'], exclude=[trial_parameters[Parameter.POSITION]])[0]
+		w7_distractor.outcome = Outcome.FAIL
+		w7_distractor.after_touch = [{'name': 'hide_other'}]
+
+		w7.add_stimulus(w7_sample)
+		w7.add_stimulus(w7_distractor)
+
+		#this is necessary for 'hide'ing to work?
+		for stimulus in w7.stimuli:
+			stimulus.auto_draw = True
+
+		# Window 8
+		w8 = Window(blank=0.5)
+		
+		# Penalty window - conditional
+		pw = Window(blank=1.5)
+
+		return [w1, w2, w3, w4, w5, w6, w7, w8, pw]
--- tasks/drst.py
+++ tasks/drst.py
@@ -3,95 +3,112 @@ from task_structure import TaskStructure
 import random
 import copy
 from time import sleep
+import logging
 
-  class TaskInterface(TaskStructure):
+logger = logging.getLogger(__name__)
+
+class TaskInterface(TaskStructure):
     def __init__(self):
-        super().__init()
+        super().__init__()
         self.init_parameters()
 
 # Defining possible stimuli
     def init_parameters(self):
-        img1 = Stimulus(shape = StimulusShape.IMAGE, size = (200,200), image = 'tasks/images/drst_img1.jpg', color = (1,1,1) size_touch = (200,200))
-        img2 = Stimulus(shape = StimulusShape.IMAGE, size = (200,200), image = 'tasks/images/drst_img2.jpg', color = (1,1,1) size_touch = (200,200))
-        img3 = Stimulus(shape = StimulusShape.IMAGE, size = (200,200), image = 'tasks/images/drst_img3.jpg', color = (1,1,1) size_touch = (200,200))
-        img4 = Stimulus(shape = StimulusShape.IMAGE, size = (200,200), image = 'tasks/images/drst_img4.jpg', color = (1,1,1) size_touch = (200,200))
-        img5 = Stimulus(shape = StimulusShape.IMAGE, size = (200,200), image = 'tasks/images/drst_img5.jpg', color = (1,1,1) size_touch = (200,200))
-        img6 = Stimulus(shape = StimulusShape.IMAGE, size = (200,200), image = 'tasks/images/drst_img6.jpg', color = (1,1,1) size_touch = (200,200))
-        img7 = Stimulus(shape = StimulusShape.IMAGE, size = (200,200), image = 'tasks/images/drst_img7.jpg', color = (1,1,1) size_touch = (200,200))
-        img8 = Stimulus(shape = StimulusShape.IMAGE, size = (200,200), image = 'tasks/images/drst_img8.jpg', color = (1,1,1) size_touch = (200,200))
-        img9 = Stimulus(shape = StimulusShape.IMAGE, size = (200,200), image = 'tasks/images/drst_img9.jpg', color = (1,1,1) size_touch = (200,200))
-        stimulus_list = [img1, img2, img3, img4, img5, img6, img7, img8, img9]
+        stimulus_list = [
+            Stimulus(shape = StimulusShape.IMAGE, size = (200,200), image = 'tasks/images/composite1-1.jpg', color = (1,1,1), size_touch = (200,200)),
+            Stimulus(shape = StimulusShape.IMAGE, size = (200,200), image = 'tasks/images/composite1-2.jpg', color = (1,1,1), size_touch = (200,200)),
+            Stimulus(shape = StimulusShape.IMAGE, size = (200,200), image = 'tasks/images/composite2-1.jpg', color = (1,1,1), size_touch = (200,200)),
+            # Stimulus(shape = StimulusShape.IMAGE, size = (200,200), image = 'tasks/images/composite2-2.jpg', color = (1,1,1), size_touch = (200,200)),
+            # Stimulus(shape = StimulusShape.IMAGE, size = (200,200), image = 'tasks/images/drst_img5.jpg', color = (1,1,1), size_touch = (200,200)),
+            # Stimulus(shape = StimulusShape.IMAGE, size = (200,200), image = 'tasks/images/drst_img6.jpg', color = (1,1,1), size_touch = (200,200)),
+            # Stimulus(shape = StimulusShape.IMAGE, size = (200,200), image = 'tasks/images/drst_img7.jpg', color = (1,1,1), size_touch = (200,200)),
+            # Stimulus(shape = StimulusShape.IMAGE, size = (200,200), image = 'tasks/images/drst_img8.jpg', color = (1,1,1), size_touch = (200,200)),
+            # Stimulus(shape = StimulusShape.IMAGE, size = (200,200), image = 'tasks/images/drst_img9.jpg', color = (1,1,1), size_touch = (200,200)),
+        ]
+        self.stimulus_count = len(stimulus_list)
+
         self.add_parameter(Parameter.TARGET, stimulus_list)
     # Defining possible positions
-        position_list = [(0,0), (-400, 0), (400, 0), (-400, -400), (-400, 400), (400, 400), (400, -400), (0, 400), (0, -400)]
+        position_list = [(0,0), (-200, 0), (200, 0), (-200, -200)]
+        #, (-200, 200), (200, 200), (200, -200), (0, 200), (0, -200)]
         self.add_parameter(Parameter.POSITION, position_list)
+        
+        # self.add_parameter(Parameter.TARGET_NUMBER, 1)
+        # self.add_parameter(Parameter.DISTRACTOR_NUMBER, 3)
     # Defining delay between stimuli?
-        delay_list = [1]
-        self.add_parameter(Parameter.DELAY, delay_list, pseudorandom = False) #Don't use DELAY, probably have to create a new parameter
+        # delay_list = [1]
+        # self.add_parameter(Parameter.DELAY, delay_list, pseudorandom = False) #Don't use DELAY, probably have to create a new parameter
 
 # Generating trials based on parameters
     def generate_trials(self):
         self.trials = self.pseudorandomize_parameters()
 
 # help
-    def build_trial(self, trial_parameters={}):
-    #Window 1: cue window
+    def build_trial(self, trial_parameters={}):        
+        #Window 1: cue window
         w1 = Window(transition=WindowTransition.RELEASE)
         w1_square = Stimulus(shape=StimulusShape.SQUARE, size=(250, 250), color=(-1, -1, -1), position=(0,0))
         w1.add_stimulus(w1_square)
-
-############
-
-    #Window 2: main task window
-        w2 = Window(transition=WindowTransition.TOUCH, is_outcome=True)
-    #defining penalty stimuli
-        penalty_stim = Stimulus(shape = StimulusShape.IMAGE, image = trial_parameters[Parameter.TARGET])
-        penalty_stim.position = trial_parameters[Parameter.POSITION][0]
-        penalty_stim.outcome = Outcome.FAIL
-        penalty_stim.after_touch = [{'name': 'hide_other'}]
-    #defining reward stimuli
-        reward_stim = self.randomize_from(self.pseudorandom_parameters[Parameter.TARGET]['values'], exclude=[trial_parameters[Parameter.TARGET]])[0] 
-        reward_stim.position= self.randomize_from(self.pseudorandom_parameters[Parameter.POSITION]['values'], exclude=[trial_parameters[Parameter.POSITION]])[0] 
-        reward_stim.outcome = Outcome.SUCCESS
-        reward_stim.after_touch = [{'name': 'hide_other'}]
         
-############
+        # Build trial window sequence. Starting with no distractors, each successive window displays all prior stimuli.
+        # The last window's target stimulus becomes a distractor in all subsequent windows
+        
+        # NOTE This while loop is a workaround. The `psuedorandom_parameters()` function is unexpectedly returning
+        # a variable number of stimuli, instead of simply rearranging the list in a psuedorandom way as we expected,
+        # so we re-try the selection until we come up with the desired number of stimuli.
+        # IMPORTANT: this may not be statistically valid. 
+        stimuli = []
+        while len(stimuli) != self.stimulus_count:
+            stimuli = self.randomize_from(self.pseudorandom_parameters[Parameter.TARGET]['values'])
+            # deep copy all stimuli per README.md
+            stimuli = [copy.copy(stimulus) for stimulus in stimuli]
+            stimuli_positions = self.randomize_from(self.pseudorandom_parameters[Parameter.POSITION]['values'], size=len(stimuli))
+            print("="*60)
+            logger.info(f"stimuli {len(stimuli)}")
+            logger.info(f"positions {len(stimuli_positions)}")
+            print("="*60)
 
-    #populating window with stimuli
-        current_objects = 0
-        distractor_index = 8
-        while current_objects < distractor_index:
-            w2.add_stimulus(penalty_stim)
-            current_objects += 1 
-            sleep(1)
-            if current_objects == distractor_index:
-               w2.add_stimulus(reward_stim)
-        #sleep()?
+        trial_sequence = []
+        distractors = []
+        for (stimulus, position) in zip(stimuli, stimuli_positions):
+            stimulus.position = position
+            target = stimulus
+            target.outcome = Outcome.SUCCESS
+            window = Window(transition=WindowTransition.TOUCH, is_outcome=False)
+            for distractor in distractors:
+                window.add_stimulus(distractor)
+            window.add_stimulus(target)
+            trial_sequence.append(window)
+        
+            # Set up distractor for subsequent window:
+            distractor = copy.copy(target)
+            distractor.outcome = Outcome.FAIL
+            distractors.append(distractor)    
 
-##############
+        trial_sequence[-1].is_outcome = True
 
-        #necessary for 'hide'ing to work?
-        for stimulus in w2.stimuli:
-            stimulus.auto_draw = True
-
-        #Window 3: Spacer
-        w3 = Window(blank=0.5)
+        # Spacer
+        spacer = Window(blank=0.5)
 
         # Penalty window
         pw = Window(blank=1)
 
-        return [w1, w2, w3, pw]
-
+        return [w1] + trial_sequence + [spacer, pw]
 
 
 # What I want for this task:
-    # Main task has 9 possible positions for targets to appear 
-    # targets will appear one at a time with a 1 second delay in between each, at the 9 possible positions 
-    # both targets and positions will be randomized
-    # all targets except for the last one will be distractors/result in a penalty when touched
-    # any stimulus can be touched at any time, but only touching the last one that appears will result in a success outcone
-
-
-# Questions
-    #copy.copy()?
+    # TODO update this to reflect revised understanding
     
+    # random targets from a list of possible targets will appear one at a time
+    #   at a random position from a list of 6 possible positions 
+    
+    # TODO 2 seconds of a blank window between each stimulus window (each novel stimulus appearing)
+        # could be pseudorandomized per trial
+    
+    # TODO maybe have a long timeout period? Paper says 12s, but try 5s?
+    # TODO reward after each correct touch?
+    
+    # all targets except for the novel one one will be distractors/result in a penalty when touched
+    # any stimulus can be touched at any time, but only touching the novel one that appears will result in a success outcone
+    
+    # TODO make list comprehension for stimulus list
