4ca7c5edd26caf4bb069fe508bac09b65ab12217
Brian Stewart <brian.stewart@nih.gov>
2024-02-23 18:16:26 -0500

    Fix RPi MAC script

--- scripts/get_serial_and_mac.sh
+++ scripts/get_serial_and_mac.sh
@@ -4,11 +4,11 @@
 wireless_interface="wlan0"
 
 # Get wireless MAC address
-wireless_mac=$(ifconfig $wireless_interface | grep HWaddr | awk '{print $NF}')
+wireless_mac=$(ifconfig $wireless_interface | grep ether | awk '{print $2}')
 
 # Get serial number from CPU info
 serial_number=$(cat /proc/cpuinfo | grep Serial | awk '{print $3}')
 
 # Print results
-echo "Wireless MAC address: $wireless_mac"
-echo "Serial number: $serial_number"
+echo "Wireless MAC:	$wireless_mac"
+echo "Serial number:	$serial_number"
