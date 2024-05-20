bde2268fa7f495cc586d0fe219243cd99115ae95
sccn@ccpc06 <sccn@ccpc06>
2024-05-20 12:00:49 -0400

    Add/update .env configuration

--- bin/callicog.sh
+++ bin/callicog.sh
@@ -1,3 +1,9 @@
-source ~/callicog/callicog.env      &&  \
-source $CALLICOG_VENV/bin/activate  &&  \
+#!/bin/bash
+
+# This script is just a convenience, it activates the virtual
+# environment and then invokes the `callicog` command,
+# passing through any arguments supplied.
+
+source $HOME/callicog/.env		&&  \
+source $CALLICOG_VENV/bin/activate	&&  \
 callicog "$@"
--- bin/server.sh
+++ bin/server.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# This script simply starts the callicog task agent (aka 'the listener').
+
+echo 'DEPRECATION PENDING: this script will be removed soon.'
+# Now that the CLI interface has been improved, it is preferable to
+# directly invoke these commands where needed, rather than obscuring
+# them in an additional script layer.
+
 # This may be needed for the Intel NUCs
 #xrandr --output HDMI-2 --mode "1280x720"
-source ~/callicog/callicog.env      &&  \
+source $HOME/callicog/.env      &&  \
 source $CALLICOG_VENV/bin/activate  &&  \
 callicog start agent --port=ttyACM0 --width=1280 --height=720 --dummy
