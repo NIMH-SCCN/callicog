f09b68f4992dc0e0d2ab9b4b9f03b23366216b26
Brian Stewart <bstewart@usnews.com>
2024-05-29 16:36:13 -0400

    Make IMAGE_DIR relative path
    This gets evaluated on the executive computer (Mac) before the task
    is pickled and sent to the task agent (Linux). If we use the absolute
    path, we end up with something like "/Users/sccn/...", which is not the
    correct path on Linux, it would be "/home/sccn/...". Let's see if we can
    avoid this just using relative path.

--- src/callicog/tasks/__init__.py
+++ src/callicog/tasks/__init__.py
@@ -1,12 +1,4 @@
-import os
 import pathlib
 
 
-_this_dir = pathlib.Path(__file__).parent.resolve()
-IMAGE_DIR = os.environ.get("CALLICOG_TASK_IMAGE_DIR") or _this_dir/"images"
-
-
-if __name__ == "__main__":
-    # TODO delete this after troubleshooting the .env situation.
-    print(IMAGE_DIR)
-    import pdb; pdb.set_trace()
+IMAGE_DIR = pathlib.Path(__file__).parent/"images"
