493672ae6741b0b5f5b5489eab3fe82fe543fcbe
jackscott1 <jack.scott2@nih.gov>
2024-10-28 13:11:30 -0400

    test

--- src/callicog/trial_interface.py
+++ src/callicog/trial_interface.py
@@ -157,8 +157,10 @@ class WindowRuntime:
         print(ppy_mouse.getPressed()[0])
         while not (ppy_mouse.getPressed()[0] or ppy_mouse.getPressed()[1] or ppy_mouse.getPressed()[2]):
             time.sleep(0.001)
-            if ppy_mouse.getPressed():
-                    print(ppy_mouse.getPressed())
+            if ppy_mouse.getPressed()[1]:
+                    print('1')
+            if ppy_mouse.getPressed()[2]:
+                    print('2')
             
             if window.active_timeout > 0 and (datetime.now() - start).total_seconds() > window.active_timeout: #TODO: the variable 'start' is refreshed after each touch, so touching outside stimuli resets timeout - this behavior could be improved
                 return 0, 0, True
